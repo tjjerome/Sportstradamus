e846d0bc3316c297688b7a4bb69adc40abdd1aaa
Trevor Jerome <trevor81994@gmail.com>
2023-07-17 09:12:50 -0500

    update NFL archive

--- src/sportstradamus/test.py
+++ src/sportstradamus/test.py
@@ -1,11 +1,19 @@
 from sportstradamus.helpers import Archive
+import pickle
 
 archive = Archive(True)
 
 for market in list(archive["NFL"].keys()):
-    for date in list(archive["NFL"][market].keys()):
-        if "/" in date:
-            archive["NFL"][market][date.replace(
-                "/", "-")] = archive["NFL"][market].pop(date)
+    if market not in ["Moneyline", "Totals"]:
+        for date in list(archive["NFL"][market].keys()):
+            for player in list(archive["NFL"][market][date].keys()):
+                for line in list(archive["NFL"][market][date][player].keys()):
+                    if len(archive["NFL"][market][date][player][line]) > 4:
+                        archive["NFL"][market][date][player][line] = archive["NFL"][market][date][player][line][-4:]
 
-archive.write()
+with open('sportstradamus/data/archive_full.dat', 'wb') as outfile:
+    pickle.dump(archive.archive, outfile)
+
+archive.clip()
+with open('sportstradamus/data/archive.dat', 'wb') as outfile:
+    pickle.dump(archive.archive, outfile)
