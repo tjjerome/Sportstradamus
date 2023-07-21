b9cd10b9af43defe6bd686c0ded1dff643cfdc54
Trevor Jerome <trevor81994@gmail.com>
2023-07-20 19:48:00 -0500

    found NFL abbreviations

--- src/sportstradamus/helpers.py
+++ src/sportstradamus/helpers.py
@@ -374,23 +374,32 @@ mlb_pitchers["ANA"] = mlb_pitchers.get("LAA", "")
 mlb_pitchers["ARI"] = mlb_pitchers.get("AZ", "")
 mlb_pitchers["WAS"] = mlb_pitchers.get("WSH", "")
 
-urls = [  # "http://site.api.espn.com/apis/site/v2/sports/football/nfl/teams",
-    "http://site.api.espn.com/apis/site/v2/sports/baseball/mlb/teams",
-    "http://site.api.espn.com/apis/site/v2/sports/hockey/nhl/teams",
-    "http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams",
-]
+urls = ["http://site.api.espn.com/apis/site/v2/sports/football/nfl/teams",
+        "http://site.api.espn.com/apis/site/v2/sports/baseball/mlb/teams",
+        "http://site.api.espn.com/apis/site/v2/sports/hockey/nhl/teams",
+        "http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams",
+        ]
 
 abbreviations = {}
 for url in urls:
-    res = scraper.get(url)["sports"][0]["leagues"][0]
-    league = res["abbreviation"]
-    if league not in abbreviations:
-        abbreviations[league] = {}
-    for team in res["teams"]:
-        name = remove_accents(team["team"]["displayName"])
-        abbr = team["team"]["abbreviation"]
-        abbreviations[league][name] = abbr
-
-abbreviations["NBA"]["Los Angeles Clippers"] = "LAC"
-abbreviations["NHL"]["St Louis Blues"] = "STL"
-# abbreviations['NFL']['Washington Football Team'] = 'WSH'
+    res = requests.get(url)
+    try:
+        res = res.json()
+    except:
+        continue
+    if 'sports' in res:
+        res = res["sports"][0]["leagues"][0]
+        league = res["abbreviation"]
+        if league not in abbreviations:
+            abbreviations[league] = {}
+        for team in res["teams"]:
+            name = remove_accents(team["team"]["displayName"])
+            if name == "Los Angeles Clippers":
+                abbr = "LAC"
+            elif name == "St Louis Blues":
+                abbr = "STL"
+            elif name == "Washington Football Team":
+                abbr = "WSH"
+            else:
+                abbr = team["team"]["abbreviation"]
+            abbreviations[league][name] = abbr
