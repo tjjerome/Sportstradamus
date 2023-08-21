40f427933624f511d85981c22787ecadddc4832c
Trevor Jerome <trevor81994@gmail.com>
2023-08-21 18:35:51 -0500

    add batting order to MLB stats

--- src/sportstradamus/refactor.py
+++ src/sportstradamus/refactor.py
@@ -1,15 +1,45 @@
 from sportstradamus.helpers import Archive
+from sportstradamus.stats import StatsMLB
 from datetime import datetime
 from tqdm import tqdm
+import requests
+import importlib.resources as pkg_resources
+from sportstradamus import creds, data
+import json
 
-archive = Archive(True)
+# archive = Archive("MLB")
 
-for market in ["pitcher fantasy score", "pitcher fantasy points underdog"]:
-    for date in tqdm(list(archive["MLB"][market].keys())):
-        # if datetime.strptime(date, "%Y-%m-%d") > datetime(2023, 6, 13):
-        #     continue
-        for player in list(archive["MLB"][market][date].keys()):
-            if " + " in player or " vs. " in player:
-                archive["MLB"][market][date].pop(player)
+# for market in ["batter strikeouts"]:
+#     for date in tqdm(list(archive["MLB"][market].keys())):
+#         # if datetime.strptime(date, "%Y-%m-%d") > datetime(2023, 6, 13):
+#         #     continue
+#         for player in list(archive["MLB"][market][date].keys()):
+#             if " + " in player or " vs. " in player:
+#                 continue
 
-archive.write()
+#             for line in list(archive["MLB"][market][date][player].keys()):
+#                 if line == "Closing Lines":
+#                     continue
+#                 if line > 2:
+#                     archive["MLB"][market][date][player].pop(line)
+
+#             if len(archive["MLB"][market][date][player].keys()) == 0:
+#                 archive["MLB"][market][date].pop(player)
+
+# archive.write()
+
+mlb = StatsMLB()
+mlb.load()
+mlb.update()
+
+offer = {
+    "Player": "Jeremy Pena",
+    "Market": "hits+runs+rbi",
+    "Date": "2023-08-22",
+    "Line": 1.5,
+    "League": "MLB",
+    "Team": "HOU",
+    "Opponent": "BOS"
+}
+
+mlb.get_stats(offer, date=offer['Date'])
--- src/sportstradamus/see_features.py
+++ src/sportstradamus/see_features.py
@@ -2,14 +2,24 @@ import pickle
 import importlib.resources as pkg_resources
 from sportstradamus import data
 import lightgbm as lgb
-
+import os.path
+import pandas as pd
 
 model_list = [f.name for f in pkg_resources.files(
     data).iterdir() if ".mdl" in f.name]
 model_list.sort()
 
+data_list = []
 for model_str in model_list:
     with open(pkg_resources.files(data) / model_str, "rb") as infile:
         model = pickle.load(infile)
 
     lgb.plot_importance(model['model'].estimator)
+
+    # size = os.path.getsize(pkg_resources.files(data) / model_str) / 1024
+    # data_list.append(model['params'] | {"size": size})
+
+    # print(f"{model_str} - Filesize: {size} KB, Max Depth: {depth}, Num Leaves: {width}")
+
+# df = pd.DataFrame(data_list)
+# df.corr()
