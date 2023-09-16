b623e6489203cba3fffd869370610dbe36b88615
Trevor Jerome <trevor81994@gmail.com>
2023-09-16 18:56:38 -0500

    update NBA abbreviations

--- src/sportstradamus/refactor.py
+++ src/sportstradamus/refactor.py
@@ -8,22 +8,22 @@ from sportstradamus import creds, data
 import json
 import numpy as np
 
-archive = Archive()
+archive = Archive("NBA")
 stats = StatsNBA()
 stats.load()
 stats.update()
 
 for league in ["NBA"]:
-    for date in tqdm(list(archive[league]["Moneyline"].keys()), desc=league):
+    for date in tqdm(list(archive[league]["Totals"].keys()), desc=league):
         if datetime.strptime(date, "%Y-%m-%d") < datetime(2022, 10, 1):
             continue
         market = "Totals"
         new_dict = {}
         for team in list(archive[league][market][date].keys()):
-            if team == "UTAH":
-                team = "UTA"
             opponent = stats.gamelog.loc[(stats.gamelog["GAME_DATE"].str[:10] == date) & (
                 stats.gamelog["TEAM_ABBREVIATION"] == team), "OPP"].max()
+            if opponent is not str:
+                continue
             new_dict.update({team: archive[league][market][date][opponent]})
 
         archive[league][market][date] = new_dict
