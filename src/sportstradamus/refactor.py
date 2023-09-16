eb42e666cfb1f1a8e833a2284ec4c13f5c93846c
Trevor Jerome <trevor81994@gmail.com>
2023-09-16 18:16:18 -0500

    training

--- src/sportstradamus/refactor.py
+++ src/sportstradamus/refactor.py
@@ -1,5 +1,5 @@
 from sportstradamus.helpers import Archive
-from sportstradamus.stats import StatsNFL
+from sportstradamus.stats import StatsNBA
 from datetime import datetime
 from tqdm import tqdm
 import requests
@@ -8,19 +8,24 @@ from sportstradamus import creds, data
 import json
 import numpy as np
 
-archive = Archive("All")
+archive = Archive()
+stats = StatsNBA()
+stats.load()
+stats.update()
 
-for league in ["NFL", "NBA", "NHL", "MLB"]:
+for league in ["NBA"]:
     for date in tqdm(list(archive[league]["Moneyline"].keys()), desc=league):
-        for market in ["Moneyline", "Totals"]:
-            if "CHW" in archive[league][market][date]:
-                archive[league][market][date]["CWS"] = archive[league][market][date].pop(
-                    "CHW")
-            if "WSH" in archive[league][market][date]:
-                archive[league][market][date]["WAS"] = archive[league][market][date].pop(
-                    "WSH")
-            if "AZ" in archive[league][market][date]:
-                archive[league][market][date]["ARI"] = archive[league][market][date].pop(
-                    "AZ")
+        if datetime.strptime(date, "%Y-%m-%d") < datetime(2022, 10, 1):
+            continue
+        market = "Totals"
+        new_dict = {}
+        for team in list(archive[league][market][date].keys()):
+            if team == "UTAH":
+                team = "UTA"
+            opponent = stats.gamelog.loc[(stats.gamelog["GAME_DATE"].str[:10] == date) & (
+                stats.gamelog["TEAM_ABBREVIATION"] == team), "OPP"].max()
+            new_dict.update({team: archive[league][market][date][opponent]})
+
+        archive[league][market][date] = new_dict
 
 archive.write()
