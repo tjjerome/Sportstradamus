2d655ad13f9658d38f203b6e2d6c6fd9f72efab9
Trevor Jerome <trevor81994@gmail.com>
2023-09-17 11:48:52 -0500

    bugfix

--- src/sportstradamus/test.py
+++ src/sportstradamus/test.py
@@ -1,20 +1,20 @@
 from sportstradamus.stats import StatsNFL, StatsMLB, StatsNBA, StatsNHL
 from datetime import datetime
 
-NFL = StatsNFL()
-NFL.season_start = datetime(2020, 9, 1)
-NFL.update()
-NFL.season_start = datetime(2021, 9, 1)
-NFL.update()
-NFL.season_start = datetime(2022, 9, 1)
-NFL.update()
-NFL.season_start = datetime(2023, 9, 1)
-NFL.update()
-
 # NFL = StatsNFL()
-# NFL.load()
-# NFL.profile_market('passing yards')
-# print(NFL.defenseProfile)
+# NFL.season_start = datetime(2020, 9, 1)
+# NFL.update()
+# NFL.season_start = datetime(2021, 9, 1)
+# NFL.update()
+# NFL.season_start = datetime(2022, 9, 1)
+# NFL.update()
+# NFL.season_start = datetime(2023, 9, 1)
+# NFL.update()
+
+NFL = StatsNFL()
+NFL.load()
+NFL.update()
+NFL.get_fantasy()
 
 # MLB = StatsMLB()
 # MLB.load()
