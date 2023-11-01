bf31ca6e52ffb6787efdbfc6e033b501d13e1de0
Trevor Jerome <trevor81994@gmail.com>
2023-11-01 18:55:23 -0500

    update archive

--- src/sportstradamus/refactor.py
+++ src/sportstradamus/refactor.py
@@ -21,29 +21,24 @@ for league in leagues:
     markets.remove("Moneyline")
     markets.remove("Totals")
     for market in markets:
-        # cv = stat_cv[league][market]
+        cv = stat_cv[league].get(market, 1)
         for date in list(archive[league][market].keys()):
             for player in list(archive[league][market][date].keys()):
                 lines = list(archive[league][market][date][player].keys())
                 if "Closing Lines" in archive[league][market][date][player]:
                     lines.remove("Closing Lines")
-                    # EV = []
-                    # for i, book in enumerate(archive[league][market][date][player]["Closing Lines"]):
-                    #     if book is not None:
-                    #         ev = get_ev(float(book["Line"]), odds_to_prob(
-                    #             int(book["Under"])), cv)
-                    #         EV.append(ev)
-                    #         archive[league][market][date][player]["Closing Lines"][i]['EV'] = ev
-                    #     else:
-                    #         EV.append(None)
-
-                for line in lines:
-                    for i, book in enumerate(archive[league][market][date][player][line]):
-                        if type(archive[league][market][date][player][line][i]) is dict:
-                            archive[league][market][date][player]["Closing Lines"][i] = archive[league][market][date][player][line][i]
-                            archive[league][market][date][player][line][i] = None
-                        # if EV[i] is not None:
-                        #     archive[league][market][date][player][line][i] = norm.sf(
-                        #         line, EV[i], cv*EV[i])
+                    EV = []
+                    for i, book in enumerate(archive[league][market][date][player]["Closing Lines"]):
+                        if book is not None:
+                            ev = get_ev(float(book["Line"]), odds_to_prob(
+                                int(book["Under"])), cv)
+                            EV.append(ev)
+                            archive[league][market][date][player]["Closing Lines"][i]['EV'] = ev
+                        else:
+                            EV.append(None)
+                archive[league][market][date][player] = {
+                    "EV": EV,
+                    "Lines": lines
+                }
 
 archive.write()
