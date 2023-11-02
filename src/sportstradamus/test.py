2171ae12db1df6b6cf4bee72c65c855f77c31c74
Trevor Jerome <trevor81994@gmail.com>
2023-11-02 09:30:08 -0500

    updates to training

--- src/sportstradamus/test.py
+++ src/sportstradamus/test.py
@@ -20,16 +20,16 @@ NFL.update()
 NFL.season_start = datetime(2023, 9, 1).date()
 NFL.update()
 
-NBA = StatsNBA()
-NBA.season = "2021-22"
-NBA.season_start = datetime(2021, 10, 1).date()
-NBA.update()
-NBA.season = "2022-23"
-NBA.season_start = datetime(2022, 10, 1).date()
-NBA.update()
-NBA.season = "2023-24"
-NBA.season_start = datetime(2023, 10, 1).date()
-NBA.update()
+# NBA = StatsNBA()
+# NBA.season = "2021-22"
+# NBA.season_start = datetime(2021, 10, 1).date()
+# NBA.update()
+# NBA.season = "2022-23"
+# NBA.season_start = datetime(2022, 10, 1).date()
+# NBA.update()
+# NBA.season = "2023-24"
+# NBA.season_start = datetime(2023, 10, 1).date()
+# NBA.update()
 
 # NHL = StatsNHL()
 # NHL.season_start = datetime(2021, 10, 1).date()
