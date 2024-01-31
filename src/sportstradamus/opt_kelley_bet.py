1e346f8418eac8c15851e4162b95d49ed5686eef
Trevor Jerome <trevor81994@gmail.com>
2024-01-30 20:52:16 -0600

    add recommended bet size based on Kelley Formula

--- src/sportstradamus/opt_kelley_bet.py
+++ src/sportstradamus/opt_kelley_bet.py
@@ -7,41 +7,10 @@ import importlib.resources as pkg_resources
 from sportstradamus import data, creds
 from sportstradamus.stats import StatsNBA, StatsMLB, StatsNHL, StatsNFL
 from tqdm import tqdm
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.oauth2.credentials import Credentials
-from google.auth.transport.requests import Request
-import gspread
+from matplotlib import pyplot as plt
+import seaborn as sns
 
 def reflect():
-    # Authorize the gspread API
-    SCOPES = [
-        "https://www.googleapis.com/auth/drive",
-        "https://www.googleapis.com/auth/drive.file",
-    ]
-    cred = None
-
-    # Check if token.json file exists and load credentials
-    if os.path.exists((pkg_resources.files(creds) / "token.json")):
-        cred = Credentials.from_authorized_user_file(
-            (pkg_resources.files(creds) / "token.json"), SCOPES
-        )
-
-    # If no valid credentials found, let the user log in
-    if not cred or not cred.valid:
-        if cred and cred.expired and cred.refresh_token:
-            cred.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                (pkg_resources.files(creds) / "credentials.json"), SCOPES
-            )
-            cred = flow.run_local_server(port=0)
-
-        # Save the credentials for the next run
-        with open((pkg_resources.files(creds) / "token.json"), "w") as token:
-            token.write(cred.to_json())
-
-    gc = gspread.authorize(cred)
-
     tqdm.pandas()
     payout_table = {
         "Underdog": [(1, 1),
@@ -57,6 +26,10 @@ def reflect():
                     (10, 0, 0, 0, 0),
                     (10, 2, 0.4, 0, 0, 0),
                     (25, 2, 0.4, 0, 0, 0, 0)]
+    }
+    pt_simp = { # using equivalent payouts when insured picks are better
+        "Underdog": [0, 0, 3, 6, 10.9, 20.2],
+        "PrizePicks": [0, 0, 3, 5.3, 10, 20.8, 38.8]
     }
 
     nba = StatsNBA()
@@ -163,58 +136,27 @@ def reflect():
     parlays.dropna(subset=['Legs'], inplace=True)
     parlays[["Legs", "Misses"]] = parlays[["Legs", "Misses"]].astype(int)
     parlays["Profit"] = parlays.apply(lambda x: np.clip(payout_table[x.Platform][x.Legs][x.Misses]*(x.Boost if x.Boost < 2 or x.Misses==0 else 1),None,100)-1, axis=1)
-    parlays["Profit"] = parlays["Profit"]*parlays["Rec Bet"]
 
     pd.concat([parlays, parlays_clean]).drop_duplicates(subset=parlays.columns[:-3]).to_pickle(filepath)
 
-    parlays.loc[parlays["Model EV"] >= 6, "Model EV"] = 5.99
+    parlays["Actual Legs"] = parlays.apply(lambda x: np.sum([1 for y in x.index if "Leg " in y and x[y] != ""]), axis=1)
 
-    profits = {}
-    masks = {}
+    units = []
+    profits = []
+    unit_size = np.arange(0.01, 0.1, .001)
+    control = parlays.sort_values("Model EV", ascending=False).groupby(["Game", "Date", "Platform"]).apply(lambda x: x["Profit"].head(5).mean()).sum()
+    for unit in tqdm(unit_size):
+        parlays["Bet"] = (parlays["Model EV"] - 1)/(parlays.apply(lambda x: pt_simp[x["Platform"]][x["Actual Legs"]]*x["Boost"] - 1, axis=1))
+        # unit = parlays.loc[parlays["Profit"] > 0, "Bet"].mean()
+        parlays["Bet"] = np.round(parlays["Bet"]/unit*2)/2
+        parlays["Actual Profit"] = parlays["Profit"]*parlays["Bet"]
+        profits.append(parlays.sort_values("Model EV", ascending=False).groupby(["Game", "Date", "Platform"]).apply(lambda x: x["Actual Profit"].head(5).mean()).sum())
 
-    for platform in payout_table.keys():
-        platform_df = parlays.loc[parlays["Platform"] == platform]
-        for league in ["All", "NBA", "NFL", "NHL", "MLB"]:
-            if league == "All":
-                league_df = platform_df
-            else:
-                league_df = platform_df.loc[platform_df["League"] == league]
+    plt.plot(unit_size, profits)
+    plt.plot(unit_size, np.ones(len(profits))*control, '-')
+    plt.xlabel("Unit Size")
+    plt.ylabel("Profit")
 
-            if not league_df.empty:
-                profits.setdefault(f"{platform}, {league}", {})
-
-                if platform == "Underdog":
-                    n = .01
-                    # mean_profit = np.zeros([int(1/n), int(5/n)])
-                    # for i, bt in enumerate(tqdm(np.arange(1, 2, n))):
-                    #     for j, mt in enumerate(np.arange(1, 6, n)):
-                    #         test_df = league_df.loc[(league_df["Books EV"] > bt) & (league_df["Model EV"] > mt)]
-                    #         bets = test_df.sort_values("Model EV", ascending=False).groupby(["Game", "Date"]).apply(lambda x: x.Profit.head(5).mean())
-                    #         if len(bets):
-                    #             mean_profit[i, j] = bets.sum()
-                            
-                    # model_threshold = np.argmax((mean_profit-np.nanmean(mean_profit))/np.nanstd(mean_profit) >= 1, axis=1)*n+1
-                    # book_threshold = np.arange(1,2,n)
-                    # book_threshold = book_threshold[model_threshold > 1]
-                    # model_threshold = model_threshold[model_threshold > 1]
-                    # p = np.polyfit(np.log(6-model_threshold), book_threshold, 1)
-                    # masks[league] = list(p)
-
-            for tf, days in [("Last Week", 7), ("Last Month", 30), ("Three Months", 91), ("Six Months", 183), ("Last Year", 365)]:
-                df = league_df.loc[pd.to_datetime(league_df.Date).dt.date > datetime.today().date() - timedelta(days=days)]
-
-                if not df.empty:
-                    profits[f"{platform}, {league}"][tf] = df.sort_values("Model EV", ascending=False).groupby(["Game", "Date"]).apply(lambda x: x.Profit.head(5).mean()).sum()
-
-    # filepath = pkg_resources.files(data) / "parlay_masks.json"
-    # with open(filepath, 'w') as of:
-    #     json.dump(masks, of, indent=4)
-
-    profits = pd.DataFrame(profits).T.reset_index(names='Split')
-    wks = gc.open("Sportstradamus").worksheet("Parlay Profit")
-    wks.clear()
-    wks.update([profits.columns.values.tolist()] +
-            profits.values.tolist())
     
 if __name__ == "__main__":
     reflect()
