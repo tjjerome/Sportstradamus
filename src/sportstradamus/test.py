fcae3b7e39b8cdf54115e7ac2add786b0b2fbc28
Trevor Jerome <trevor81994@gmail.com>
2024-05-13 20:50:22 -0500

    nfl player comps

--- src/sportstradamus/test.py
+++ src/sportstradamus/test.py
@@ -1,17 +1,20 @@
 from sportstradamus.stats import StatsNBA, StatsMLB, StatsNFL, StatsNHL
-from sportstradamus.helpers import scraper, archive, stat_cv
+from sportstradamus.helpers import scraper, archive, stat_cv, remove_accents
 from urllib.parse import urlencode
 from datetime import datetime, timedelta
 import importlib.resources as pkg_resources
 from sportstradamus import data
 import pickle
 import json
+import re
+import os
 from scipy.stats import norm, poisson
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
 from time import time
 import requests
+
 pd.options.mode.chained_assignment = None
 
 NFL = StatsNFL()
@@ -20,76 +23,39 @@ NFL.update()
 NFL.update_player_comps()
 NFL.load()
 
-# players = {}
-# NHL.gamelog["season"] = NHL.gamelog.gameId.astype(str).str[:4]
-# ppg = NHL.gamelog.groupby(["playerId", "season"])[["goalie fantasy points underdog", "skater fantasy points underdog"]].mean().to_dict('index')
-# for year in NHL.players.keys():
-#     for player in NHL.players[year].keys():
-#         data = NHL.players[year][player]|{"PPG": ppg[(str(player), str(year))]["goalie fantasy points underdog" if NHL.players[year][player]["position"]=="G" else "skater fantasy points underdog"]}
-#         players.setdefault(player, {}).update({f"{year}_{k}":v for k, v in data.items()})
 
-# player_df = pd.DataFrame(players).T
-
-# for position in ["C", "W", "D", "G"]:
-#     position_df = player_df.loc[player_df["2023_position"] == position].dropna(axis=1, how='all').dropna()
-#     position_df.drop(columns=[col for col in position_df.columns if any(substr in col for substr in ["name", "position", "team", "shootsCatches"])], inplace=True)
-#     C = position_df.corr()
-#     columns = list({col[5:] for col in C.columns})
-#     col_stats = {}
-#     n = len(NHL.players.keys())
-#     for col in columns:
-#         yearly = (C.loc[C.index.str.endswith(col), C.columns.str.endswith(col)]*(1-np.tri(3))).sum().sum()/n
-#         fantasy = (C.loc[C.index.str.endswith(col), C.columns.str.endswith("PPG")]*np.eye(n)).sum().sum()/n
-#         volume = (C.loc[C.index.str.endswith(col), C.columns.str.endswith("timePerGame")]*np.eye(n)).sum().sum()/n
-
-#         col_stats[col] = {
-#             "yearly": yearly,
-#             "fantasy": fantasy,
-#             "volume": volume
-#         }    
-#     col_df = pd.DataFrame(col_stats).T.sort_values("volume", ascending=False)
-#     pass
-
-
-markets = ["fantasy points prizepicks", "PTS", "REB", "AST", "TOV", "BLK", "STL"]
-
-gamelog = NBA.gamelog.loc[pd.to_datetime(NBA.gamelog["GAME_DATE"]).dt.date > (datetime.today().date() - timedelta(days=300))]
+gamelog = NFL.gamelog.loc[pd.to_datetime(NFL.gamelog["gameday"]).dt.date > (datetime.today().date() - timedelta(days=300))]
 
 gameRecords = []
 gameRecords1 = []
 gameRecords2 = []
 for i, game in tqdm(gamelog.iterrows(), desc="Checking Player Comps", total=len(gamelog)):
-    gameDate = pd.to_datetime(game["GAME_DATE"]).date()
-    if 11 >= gameDate.month >= 10:
+    gameDate = pd.to_datetime(game["gameday"]).date()
+    if 9 >= gameDate.month >= 8:
         continue
-    dateMask = (pd.to_datetime(NBA.gamelog["GAME_DATE"]).dt.date > (gameDate - timedelta(days=300))) & (pd.to_datetime(NBA.gamelog["GAME_DATE"]).dt.date < gameDate)
+    dateMask = (pd.to_datetime(NFL.gamelog["gameday"]).dt.date > (gameDate - timedelta(days=300))) & (pd.to_datetime(NFL.gamelog["gameday"]).dt.date < gameDate)
 
-    if not NBA.comps[game["POS"]].get(game["PLAYER_NAME"]):
+    if not NFL.comps[game["position group"]].get(game["player display name"]):
         continue
 
-    # if game["position"] == "G":
-    #     markets = ["goalie fantasy points underdog", "saves", "goalsAgainst"]
-    # else:
-    #     markets = ["skater fantasy points underdog", "shots", "points", "timeOnIce", "blocked"]
+    if game["position group"] == "QB":
+        markets = ["fantasy points underdog", "passing yards", "qb tds", "sacks taken", "attempts"]
+    elif game["position group"] == "RB":
+        markets = ["fantasy points underdog", "yards", "tds", "carries"]
+    else:
+        markets = ["fantasy points underdog", "yards", "tds", "targets"]
 
-    defenseVsLeague = NBA.gamelog.loc[dateMask & (NBA.gamelog["OPP"]==game["OPP"])]
-    compVsLeague = NBA.gamelog.loc[dateMask & NBA.gamelog["PLAYER_NAME"].isin(NBA.comps[game["POS"]].get(game["PLAYER_NAME"], []))]
-    defenseVsPos = defenseVsLeague.loc[defenseVsLeague["POS"] == game["POS"]]
-    defenseVsComp = defenseVsLeague.loc[defenseVsLeague["PLAYER_NAME"].isin(NBA.comps[game["POS"]].get(game["PLAYER_NAME"], []))]
-    defenseVsPlayer = defenseVsLeague.loc[defenseVsLeague["PLAYER_NAME"] == game["PLAYER_NAME"]]
-    posVsLeague = NBA.gamelog.loc[NBA.gamelog["POS"] == game["POS"]]
-
-    defenseVsLeague = defenseVsLeague[markets].div(defenseVsLeague["MIN"], axis=0)
-    compVsLeague = compVsLeague[markets].div(compVsLeague["MIN"], axis=0)
-    defenseVsPos = defenseVsPos[markets].div(defenseVsPos["MIN"], axis=0)
-    defenseVsComp = defenseVsComp[markets].div(defenseVsComp["MIN"], axis=0)
-    defenseVsPlayer = defenseVsPlayer[markets].div(defenseVsPlayer["MIN"], axis=0)
-    posVsLeague = posVsLeague[markets].div(posVsLeague["MIN"], axis=0)
+    defenseVsLeague = NFL.gamelog.loc[dateMask & (NFL.gamelog["opponent"]==game["opponent"])]
+    compVsLeague = NFL.gamelog.loc[dateMask & NFL.gamelog["player display name"].isin(NFL.comps[game["position group"]].get(game["player display name"], []))]
+    defenseVsPos = defenseVsLeague.loc[defenseVsLeague["position group"] == game["position group"]]
+    defenseVsComp = defenseVsLeague.loc[defenseVsLeague["player display name"].isin(NFL.comps[game["position group"]].get(game["player display name"], []))]
+    defenseVsPlayer = defenseVsLeague.loc[defenseVsLeague["player display name"] == game["player display name"]]
+    posVsLeague = NFL.gamelog.loc[NFL.gamelog["position group"] == game["position group"]]
         
-    defenseResult = (defenseVsComp.mean()/defenseVsLeague.mean().replace(0, np.inf)).to_dict()
-    compResult = (defenseVsComp.mean()/compVsLeague.mean().replace(0, np.inf)).to_dict()
+    defenseResult = (defenseVsComp[markets].mean()/defenseVsLeague[markets].mean().replace(0, np.inf)).fillna(0).to_dict()
+    compResult = (defenseVsComp[markets].mean()/compVsLeague[markets].mean().replace(0, np.inf)).fillna(0).to_dict()
     
-    gameResult = (game[markets]/game["MIN"]).to_dict()
+    gameResult = game[markets].fillna(0).to_dict()
     gameRecords2.append(gameResult|
                         {"comp_"+k:v for k, v in compResult.items()}|
                         {"def_"+k:v for k, v in defenseResult.items()})
