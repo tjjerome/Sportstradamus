d55ff84a1f589fb853d0b6c6c7715ea11b12f9fd
Trevor Jerome <trevor81994@gmail.com>
2025-03-27 21:19:09 -0500

    fix edge calc

--- src/sportstradamus/sportstradamus.py
+++ src/sportstradamus/sportstradamus.py
@@ -945,7 +945,7 @@ def model_prob(offers, league, market, platform, stat_data, playerStats):
         offer_df.loc[(offer_df["Bet"] == "Under"), "Books"] = (1 - offer_df.loc[(offer_df["Bet"] == "Under"), "Books"])
         offer_df["Books"] = offer_df["Books"].fillna(.5)
         offer_df["Payout"] = offer_df["Boost"] * (1.78 if platform == "Underdog" else 1)
-        offer_df["Edge"] = offer_df["Model"]-1/offer_df["Payout"]
+        offer_df["Edge"] = offer_df["Model"]/offer_df["Boost"]-1/offer_df["Payout"]
         offer_df = offer_df.loc[offer_df["Boost"] <= 3.6].sort_values("Edge", ascending=False).groupby("Player").head(1)
 
         offer_df["Avg 5"] = offer_df["Avg5"]-offer_df["Line"]
